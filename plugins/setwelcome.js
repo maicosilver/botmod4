0c559f8ffaa46be70f5e6ae831a7aebcdaa121f9
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-02-10 22:04:14 -0300

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     else if (isOwner) conn.welcome = text
     else global.DATABASE._data.chats.sWelcome = text
     m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)')
-  } else throw 'Teksnya mana?'
+  } else throw 'Onde está o texto?'
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner', 'group']
